f3f97f869e94ed589dec20497c98ffbc458982d5
IshantRaut <ishjee2018@gmail.com>
2025-11-14 19:03:39 +0530

    Refresh Access Token

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,7 +8,8 @@ import {User} from "../models/user.model.js";
 import {uploadOnCloudinary} from "../middlewares/cloudinary.js";
 // Import the ApiResponse utility for creating standardized API success responses.
 import {ApiResponse} from "../utils/ApiResponse.js";
-
+//Import Json Web Token
+import jwt from "jsonwebtoken";
 // A helper function to generate new access and refresh tokens for a given user.
 const generateAccessAndRefereshTokens = async(userId) =>{
     try {
@@ -31,6 +32,49 @@ const generateAccessAndRefereshTokens = async(userId) =>{
            throw new ApiError(500, "Something went wrong while generating referesh and access token")
     }
 }
+
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+    const  incomingRefreshToken= req.cookies.refreshToken || req.body.refreshToken;
+    if(!incomingRefreshToken){
+        throw new ApiError(401,"unauthorized request");
+    }
+
+    try {
+         const decodedToken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        )
+    const user = await User.findById(decodedToken?._id)
+    if(!user){
+        throw new ApiError(401,"Invalid refresh Token")
+    }
+     if (incomingRefreshToken !== user?.refreshToken) {
+            throw new ApiError(401, "Refresh token is expired or used")
+            
+        }
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+    
+        const {accessToken, newRefreshToken} = await generateAccessAndRefereshTokens(user._id)
+         return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(
+                200, 
+                {accessToken, refreshToken: newRefreshToken},
+                "Access token refreshed"
+            )
+        )
+    } catch (error) {
+            throw new ApiError(401, error?.message || "Invalid refresh token")
+    }
+})
+
 
 // Controller function for handling user registration. Wrapped in asyncHandler for error handling.
 const registerUser = asyncHandler( async (req, res) => {
@@ -179,5 +223,6 @@ const logoutUser = asyncHandler(async(req, res) => {
 export {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 // Import the 'Router' class from the 'express' library to create a new router object.
 import { Router } from "express"; 
 // Import the 'registerUser' controller function, which will handle the logic for user registration.
-import { registerUser,loginUser,logoutUser } from "../controllers/user.controller.js"; 
+import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controllers/user.controller.js"; 
 // Import the 'upload' middleware from 'multer.middleware.js'. This is configured to handle multipart/form-data, which is primarily used for uploading files.
 import {upload} from "../middlewares/multer.middleware.js"; 
 
@@ -27,5 +27,6 @@ router.route("/register").post( // This route will respond to HTTP POST requests
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 
+router.route("/refresh-token").post(refreshAccessToken)
 // Export the router as the default export of this module, so it can be imported and used in the main application file (e.g., app.js or index.js).
 export default router; 
